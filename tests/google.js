c04edb04b492c3507ecca734b32a0870abfea6cf
fer-linux-yotta <feryotta2@gmail.com>
2019-10-23 12:30:18 -0400

    Pruebas redireccionando a pantallas (3 pantallas)

--- tests/google.js
+++ tests/google.js
@@ -1,7 +1,7 @@
 module.exports = {
   '@tags': ['google'],
   'Google advanced search: Elon Musk'(browser) {
-    const mainQuery = 'Elon Musk';
+    const mainQuery = 'asfi.pdf';
 
     const mainQueryInputSelector = 'input[name="as_q"]';
     const languageDropdownOpenerSelector = '#lr_button';
@@ -16,6 +16,8 @@ module.exports = {
     const resultsPageLanguageSelector = '[aria-label="Buscar solo páginas en italiano"]';
     const resultsPageUpdateSelector = '[aria-label=" Último mes"]';
 
+    const submitButtonEnlace = 'a[href="http://www.archiviodistato.firenze.it/asfi/fileadmin/risorse/allegati_eventi_del_mese/2019_10_eventi_del%20mese.pdf"]';
+
     browser
       .url('https://www.google.com/advanced_search')
       .setValue(mainQueryInputSelector, mainQuery)
@@ -24,13 +26,14 @@ module.exports = {
       .click(lastUpdateDropdownOpenerSelector)
       .click(lastUpdateDropdownValuesSelector)
       .click(submitButtonSelector)
-      .assert.urlContains('as_q=Elon+Musk', 'Params: Query is Elon Musk')
-      .assert.urlContains('lr=lang_it', 'Params: Languaje is Italian')
-      .assert.urlContains('as_qdr=m', 'Params: Time period is last month')
+      // .assert.urlContains('as_q=Elon+Musk', 'Params: Query is Elon Musk')
+      // .assert.urlContains('lr=lang_it', 'Params: Languaje is Italian')
+      // .assert.urlContains('as_qdr=m', 'Params: Time period is last month')
+      .click(submitButtonEnlace)
 
-    browser.saveScreenshot('tests_output/google.pngc ');
-    browser.assert.visible(resultsPageQuerySelector, 'UI: Elon Musk is set in the query input');
+    browser.saveScreenshot('tests_output/google.png');
+    // browser.assert.visible(resultsPageQuerySelector, 'UI: Elon Musk is set in the query input');
     // // browser.assert.containsText(resultsPageLanguageSelector, 'Buscar solo páginas en italiano', 'UI: Language is set to Itilian');
-    browser.assert.containsText(resultsPageUpdateSelector, 'Último mes', 'UI: Last update is set to Month');
+    // browser.assert.containsText(resultsPageUpdateSelector, 'Último mes', 'UI: Last update is set to Month');
   }
 }
