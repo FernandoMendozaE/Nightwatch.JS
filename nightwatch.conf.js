451d0ba137f68ec6f50f939643814de688e2fae3
Fernando Trabajo_Yotta <feryotta2@gmail.com>
2019-11-22 11:13:56 -0400

    Configuración descargar reporte (EXITOSO)

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,6 +1,15 @@
-const chromedriver = require("chromedriver");
+const gecko = require("geckodriver");
+const selenium = require("selenium-server");
+const chrome = require("chromedriver");
 module.exports = (function (settings) {
+  // console.log('Firefox Path:\r\n', gecko.path);
   settings.test_workers = false;
-  settings.webdriver.server_path = chromedriver.path;
+  console.log('settings', settings.test_settings.default.desiredCapabilities.browserName)
+  console.log('settings', settings)
+  // console.log('Dato', settings)
+  // console.log('selenium', selenium)
+  // console.log('gecko', gecko)
+  // settings.test_settings.chrome.webdriver.server_path = chrome.path;
+  // settings.test_settings.firefox.webdriver.server_path = gecko.path;
   return settings;
 })(require("./nightwatch.json"));
--- tests/impuestos.js
+++ tests/impuestos.js
@@ -1,6 +1,12 @@
 module.exports = {
-  '@tags': ['google'],
-  'Google advanced search: Reporte Impuestos nacionales'(browser) {
+  '@tags': ['impuestos'],
+  'Google advanced search: Reporte Impuestos Nacionales'(browser) {
+
+    // Excel
+    // const clickExcel = 'a[href="/ckeditor/plugins/imageuploader/uploads/27052a412f.xlsx"]';
+    // Pdf
+    const clickPdf = 'a[href="/pdf/ACTA-RENDICION-DE-CUENTAS-FINAL-2014.pdf"]';
+
     // const mainQuery = 'Elon Musk';
 
     // const mainQueryInputSelector = 'input[name="as_q"]';
@@ -13,13 +19,56 @@ module.exports = {
     // const submitButtonSelector = '.jfk-button[type="submit"]';
 
     browser
-      .url('https://www.impuestos.gob.bo/pag/Portal_Oficina_Virtual')
-      // .setValue(mainQueryInputSelector, mainQuery)
-      // .click(languageDropdownOpenerSelector)
-      // .click(languageDropdownValuesSelector)
-      // .click(lastUpdateDropdownOpenerSelector)
-      // .click(lastUpdateDropdownValuesSelector)
-      // .click(submitButtonSelector)
-      .saveScreenshot('tests_output/google.png')
+      // .url('https://www.impuestos.gob.bo/pag/Portal_Oficina_Virtual') //Excel
+      // .sclick(clickExcel) // Excel
+      .url('https://www.impuestos.gob.bo/page/61') //Pdf
+      .click(clickPdf) // Excel
+
+    // .url('https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp'); //Pdf
+    // .url('https://www.uv.es/lonjedo/esoProblemas/unidad3ecuaciones.pdf'); //Pdf
+    // .url('http://www.bib.uia.mx/tesis/pdf/014828/014828.pdf'); //Pdf
+    // .click(clickPdf) // Excel
+
+
+    // .setValue(mainQueryInputSelector, mainQuery)
+    // .pause(800000)
+    // .click(languageDropdownValuesSelector)
+    // .click(lastUpdateDropdownOpenerSelector)
+    // .click(lastUpdateDropdownValuesSelector)
+    // .click(submitButtonSelector)
+    // .saveScreenshot('tests_output/automatizacion.png');
+
+    // for (let i = 0; i < 15; i++) {
+    //   try {
+    //     browser.getText({
+    //       selector: '#aside ul li a',
+    //       index: i
+    //     }, function (result) {
+    //       if (result.status != -1) {
+    //         console.log('getText result', result);
+    //         // browser
+    //         // .pause(200000)
+    //         // .end();
+    //         //Element exists, do something
+    //       } else {
+    //         browser
+    //           // .pause(200000)
+    //           .end();
+    //       }
+    //     })
+    //   } catch (error) {
+    //     console.log('error');
+    //   }
+    // }
+    browser
+      .pause(8000)
+      .end();
+
+    // browser.getText('css selector', '#aside ul li', function (result) {
+    // console.log('getText result', result.value);
+    // browser.pause(800000);
+    // });
+    // browser.expect.element('#headingText').text.to.equal('Crear tu cuenta de Google');
+
   }
 }
