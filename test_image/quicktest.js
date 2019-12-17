18be693c7c0e86f99cf60c805ece0a3a37f0a2e8
Linux lite Yotta <feryotta2@gmail.com>
2019-12-17 14:47:58 -0400

    limpiando codigo para poder trabajar en las imagenes

--- test_image/quicktest.js
+++ test_image/quicktest.js
@@ -16,68 +16,6 @@ var gsPath = projectPath + '\\executables\\ghostScript'
 // Rewrite the ghostscript path
 pdf2png.ghostscriptPath = gsPath
 
-// // Most simple example
-// pdf2png.convert(__dirname + `/pdf/rptDeudaEntidad.pdf`, function (resp) {
-// 	if (!resp.success) {
-// 		console.log("Something went wrong: " + resp.error);
-
-// 		return;
-// 	}
-
-// 	console.log("Yayy the pdf got converted, now I'm gonna save it!");
-
-// 	fs.writeFile(`test_image/image/${ci}-${password}.png`, resp.data, function (err) {
-// 		if (err) {
-// 			console.log(err);
-// 		}
-// 		else {
-// 			console.log("The file was saved!");
-// 		}
-// 	});
-// });
-
-// // Example that returns a path
-// pdf2png.convert(__dirname + "/pdf/rptDeudaEntidad.pdf", { returnFilePath: true }, function (resp) {
-// 	if (!resp.success) {
-// 		console.log("Something went wrong: " + resp.error);
-
-// 		return;
-// 	}
-
-// 	console.log("Yayy the pdf got converted, now I'm gonna save it!");
-
-// 	var img = fs.readFileSync(resp.data);
-
-// 	fs.writeFile("test_image/example_that_returns_a_path.png", img, function (err) {
-// 		if (err) {
-// 			console.log(err);
-// 		}
-// 		else {
-// 			console.log("The file was saved!");
-// 		}
-// 	});
-// });
-
-// // Example with lower quality
-// pdf2png.convert(__dirname + "/pdf/rptDeudaEntidad.pdf", { quality: 50 }, function (resp) {
-// 	if (!resp.success) {
-// 		console.log("Something went wrong: " + resp.error);
-
-// 		return;
-// 	}
-
-// 	console.log("Yayy the pdf got converted, now I'm gonna save it!");
-
-// 	fs.writeFile("test_image/example_with_lower_quality.png", resp.data, function (err) {
-// 		if (err) {
-// 			console.log(err);
-// 		}
-// 		else {
-// 			console.log("The file was saved!");
-// 		}
-// 	});
-// });
-
 // Example with higher quality
 pdf2png.convert(__dirname + '/pdf/rptDeudaEntidad.pdf', { quality: 200 }, function(resp) {
   if (!resp.success) {
@@ -96,23 +34,3 @@ pdf2png.convert(__dirname + '/pdf/rptDeudaEntidad.pdf', { quality: 200 }, functi
     }
   })
 })
-
-// // Example using a local ghostscript installation
-// pdf2png.convert(__dirname + "/pdf/rptDeudaEntidad.pdf", { useLocalGhostscript: true }, function (resp) {
-// 	if (!resp.success) {
-// 		console.log("Something went wrong: " + resp.error);
-
-// 		return;
-// 	}
-
-// 	console.log("Yayy the pdf got converted, now I'm gonna save it!");
-
-// 	fs.writeFile("test_image/example_simple.png", resp.data, function (err) {
-// 		if (err) {
-// 			console.log(err);
-// 		}
-// 		else {
-// 			console.log("The file was saved!");
-// 		}
-// 	});
-// });
