e5dec7c035f9bc8085a942bc4a380ffb5d906a56
LinuxFreddy <linux@example.com>
2019-12-27 15:57:16 -0400

    Configuracion maquina linux lite Freddy (exitoso)

--- utilitarios/configData.js
+++ utilitarios/configData.js
@@ -1,6 +1,6 @@
 // const url = '/home/linuxlite/ProyectosLinux/Node/Nightwatch.JS'
-const url = 'c:/Users/Lopez/Desktop/Robotizacion/union/Nightwatch.JS'
-// const u = `c:\Users\Lopez\Desktop\Robotizacion\union\Nightwatch.JS`
+// const url = 'c:/Users/Lopez/Desktop/Robotizacion/union/Nightwatch.JS' //Windows Freddy
+const url = '/home/linuxlite/Nightwatch.JS' //Linux Lite Freddy
 const urlImage = 'http://200.105.146.210:4500/reconocimiento/caracter'
 
 module.exports = { url, urlImage }
