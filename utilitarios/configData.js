e078f0f6fd2ac29bbaf10ad6eda7427cd6d554b7
ServidorIdepro <feryotta2@gmail.com>
2020-01-17 17:40:21 -0400

    Configurando ruta reconocimiento-caracteres

--- utilitarios/configData.js
+++ utilitarios/configData.js
@@ -1,7 +1,7 @@
 // const url = '/home/linuxlite/ProyectosLinux/Node/Nightwatch.JS'
 // const url = 'c:/Users/Lopez/Desktop/Robotizacion/union/Nightwatch.JS' //Windows Freddy
 const url = 'c:/inetpub/wwwroot/FroddiIdepro/ServicioPeticion' //Linux Lite Freddy
-const urlImage = 'http://200.105.146.210:4500/reconocimiento/caracter'
+const urlImage = 'http://10.0.1.59:4500/reconocimiento/caracter'
 const time = '2000'
 let rutaFisa = `c:\\inetpub\\wwwroot\\FroddiIdepro\\Aplicacion\\recursos`
 
