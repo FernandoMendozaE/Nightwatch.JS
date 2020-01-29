eb64385ecb810df0873add3552128b814141bf14
Fernando Trabajo_Yotta <feryotta2@gmail.com>
2020-01-29 10:56:50 -0400

    Separacion de servicio CIC y CPOP

--- routes/index.js
+++ routes/index.js
@@ -241,54 +241,128 @@ app.post('/user/', cors(), (req, res) => {
   // console.log(req.params); //obtiene  datos del id (url) parametros
 })
 
+// Creación de Router CIC
+app.post('/cic/', cors(), (req, res) => {
+  console.log(req.body)
+  let user = (req.body.user).toLowerCase()
+  let password = req.body.password
+  let _ciCliente = req.body.ciCliente.trim()
+  let arrayCliente = _ciCliente.split('  ')
+  let ciCliente = arrayCliente[0] + arrayCliente[1]
+  // let ciCliente = req.body.ciCliente + `LP`
+  // let ciCliente = '4262302LP'
+  let codigoUsuario = req.body.codigoUsuario
+  let ruta = req.body.ruta
+  let bytes = CryptoJS.AES.decrypt(password, 'PASSWORD')
+  password = bytes.toString(CryptoJS.enc.Utf8)
+  console.log('Datos:', user, ciCliente, password)
 
-//Consulta CPOP
-// exec(
-//   `npm --varUser=${user} --varPassword=${password} --varClienteCI=${ciCliente} --varAutorizacion=${autorizacion} test -- --tag cpop`,
-//   (error, stdout, stderr) => {
-//     if (error) {
-//       console.error(`exec error: ${error}`)
-//       res.send('Error al consultar CPOP')
-//       return
-//     }
-//     console.log(`stdout: ${stdout}`)
-//     console.error(`stderr: ${stderr}`)
-//     fs.rename(
-//       './test_image/pdf/rptCertificadoCPOP.pdf',
-//       `./test_image/pdf/${ciCliente}-${user}-CPOP.pdf`,
-//       err => {
-//         if (err) throw err
-//         console.log('Nombre Editado Satisfactoriamente')
-
-//         let obj = {
-//           ciCliente: ciCliente,
-//           user: user,
-//           autorizacion: autorizacion
-//         }
-//         let listadoPorHacer = []
-//         listadoPorHacer.push(obj)
-//         let data = JSON.stringify(listadoPorHacer)
-//         fs.writeFile('file/data.json', data, err => {
-//           if (err) throw new Error('No se puedo grabar', err)
-//         })
-
-//         exec('node test_image/quicktest.js', (error, stdout, stderr) => {
-//           if (error) {
-//             console.error(`exec error: ${error}`)
-//             res.send('Error al cambiar formato pdf a png CPOP')
-//             return
-//           }
-//           res.send('Consulta exitoso') //request post
-//         })
-//       }
-//     )
-//   }
-// )
+  // Fecha
+  var f = new Date()
+  let fecha = `${f.getDate()}-${f.getMonth() + 1}-${f.getFullYear()}`
 
 
+  exec(
+    `npm --varUser=${user} --varPassword=${password} --varclienteCI=${ciCliente} test -- --tag idepro`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`)
+        res.send('Error al consultar CIC')
+        return
+      }
+      fs.rename(
+        './test_image/pdf/rptDeudaEntidad.pdf',
+        `./test_image/pdf/${ciCliente}-${codigoUsuario}-CIC-${fecha}.pdf`,
+        err => {
+          if (err) throw err
+          console.log('Nombre Editado Satisfactoriamente')
+          let obj = {
+            ciCliente: ciCliente,
+            user: codigoUsuario,
+            autorizacion: null
+          }
+          let listadoPorHacer = []
+          listadoPorHacer.push(obj)
+          let data = JSON.stringify(listadoPorHacer)
+          fs.writeFile('file/data.json', data, err => {
+            if (err) throw new Error('No se puedo grabar', err)
+          })
+
+          exec('node test_image/quicktest.js', (error, stdout, stderr) => {
+            if (error) {
+              console.error(`exec error: ${error}`)
+              res.send('Error al cambiar formato pdf a png CIC')
+              return
+            }
+            image2base64(`${config.url}/test_image/image/${ciCliente}-${codigoUsuario}-CIC-${fecha}.png`)
+              .then(responseBase64 => {
+                axios
+                  .post(
+                    config.urlImage,
+                    qs.stringify({
+                      img: responseBase64
+                    })
+                  )
+                  .then(function(response) {
+                    console.log(response)
+                    let dato = response.data.data.prediction
+                    let autorizacion = finderCIC(dato).autorizacion
+                    let obj = finderCIC(dato)
+                    console.log('Autorización:', autorizacion, obj) 
+                    
+                    // imagenes
+                    let imageNames = `${ciCliente}-${codigoUsuario}`
+
+                    // data CIC
+                    let imageNameCIC = `${imageNames}-CIC-${fecha}`
+                    let dirCIC = finderCIC(dato).carteraDIR
+                    let base64CIC = responseBase64
+
+                    exec(
+                      `move test_image\\image\\${imageNames}-CIC-${fecha}.png ${config.rutaFisa}\\${ruta}`,
+                      (error, stdout, stderr) => {
+                        if (error) {
+                          console.error(`exec error: ${error}`)
+                          res.send({
+                            Resultado: 'Archivo no encontrado para mover a la ruta.',
+                            Correcto: false
+                          })
+                          return
+                        }
+                        console.log(`stdout: ${stdout}`)
+                        console.error(`stderr: ${stderr}`)
+                        
+                        res.send({
+                          imageNameCIC,
+                          base64CIC,
+                          dirCIC,
+                          autorizacion,
+                          fecha,
+                          Resultado: 'Consulta CIC finalizadaa correctamente.',
+                          Correcto: true
+                        })
+                      }
+                    )
+
+                  })
+                  .catch(function(error) {
+                    console.log(error)
+                    res.send('Error al servivio reconociemnto de imagenes') //request post
+                  })
+              })
+              .catch(error => {
+                console.log(error) //Exepection error....
+                res.send('Error al convertir png a base64')
+              })
+          })
+        }
+      )
+    }
+  )
+})
 
 
-// Creación de Router CPO
+// Creación de Router CPOP
 app.post('/cpop/', cors(), (req, res) => {
   console.log(req.body)
   let user = req.body.user
