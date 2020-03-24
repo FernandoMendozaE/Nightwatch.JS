33d4af3b62556f4ee8f396fd93a27575abac628a
servidorFortaleza <feryotta2@gmail.com>
2020-03-24 17:26:45 -0400

    Configuracion para crear casos de prueba secos

--- utilitarios/configData.js
+++ utilitarios/configData.js
@@ -2,7 +2,7 @@
 // const url = 'c:/Users/Lopez/Desktop/Robotizacion/union/Nightwatch.JS' //Windows Freddy
 const url = 'c:/inetpub/wwwroot/FroddiIdepro/ServicioPeticion' //Servidor Idepro
 // const url = 'C:/Users/Desktop/Desktop/Robotizacion/Nightwatch.JS' //Maquina Fer
-const urlImage = 'http://10.0.1.59:4500/reconocimiento/caracter'
+const urlImage = 'http://10.17.9.172:4500/reconocimiento/caracter'
 const time = '2000'
 let rutaFisa = `c:\\inetpub\\wwwroot\\FroddiIdepro\\Aplicacion\\recursos` // Servidor idepro
 // let rutaFisa = `c:\\sistemas\\FroddiUsuario\\public\\recursos` //Maquina Fer
