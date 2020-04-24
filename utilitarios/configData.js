81ff52cf0d6c838b4699a786101024aeaec2deb8
Fernando Trabajo_Yotta <feryotta2@gmail.com>
2020-04-24 10:25:52 -0400

    Configuracion idepro configdata

--- utilitarios/configData.js
+++ utilitarios/configData.js
@@ -1,8 +1,8 @@
 // const url = '/home/linuxlite/ProyectosLinux/Node/Nightwatch.JS'
 // const url = 'c:/Users/Lopez/Desktop/Robotizacion/union/Nightwatch.JS' //Windows Freddy
-const url = '/home/froddy/ServiciosFroddy/Robotizacion' //Servidor Idepro
+const url = 'c:/inetpub/wwwroot/FroddiIdepro/ServicioPeticion' //Servidor Idepro
 // const url = 'C:/Users/Desktop/Desktop/Robotizacion/Nightwatch.JS' //Maquina Fer
-const urlImage = 'http://10.17.9.172:4500/reconocimiento/caracter'
+const urlImage = 'http://10.0.1.59:4500/reconocimiento/caracter'
 const time = '2000'
 let rutaFisa = `c:\\inetpub\\wwwroot\\FroddiIdepro\\Aplicacion\\recursos` // Servidor idepro
 // let rutaFisa = `c:\\sistemas\\FroddiUsuario\\public\\recursos` //Maquina Fer
