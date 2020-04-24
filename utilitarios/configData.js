6fb39b40ba83b9b4b56a90d1a79cb278cbb86232
Fernando Trabajo_Yotta <feryotta2@gmail.com>
2020-04-24 13:09:18 -0400

    Cambios

--- utilitarios/configData.js
+++ utilitarios/configData.js
@@ -3,7 +3,7 @@
 const url = 'c:/inetpub/wwwroot/FroddiIdepro/ServicioPeticion' //Servidor Idepro
 // const url = 'C:/Users/Desktop/Desktop/Robotizacion/Nightwatch.JS' //Maquina Fer
 const urlImage = 'http://10.0.1.59:4500/reconocimiento/caracter'
-const time = '2000'
+const time = '200000'
 let rutaFisa = `c:\\inetpub\\wwwroot\\FroddiIdepro\\Aplicacion\\recursos` // Servidor idepro
 // let rutaFisa = `c:\\sistemas\\FroddiUsuario\\public\\recursos` //Maquina Fer
 
