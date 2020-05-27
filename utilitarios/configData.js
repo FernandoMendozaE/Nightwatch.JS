c52b36deb20c4c035ada3a31ef6c1bce32d28e5a
Fernando Trabajo_Yotta <feryotta2@gmail.com>
2020-05-27 16:55:59 -0400

    Configurando cambios nuevo servidor Idepro

--- utilitarios/configData.js
+++ utilitarios/configData.js
@@ -1,11 +1,7 @@
-// const url = '/home/linuxlite/ProyectosLinux/Node/Nightwatch.JS'
-// const url = 'c:/Users/Lopez/Desktop/Robotizacion/union/Nightwatch.JS' //Windows Freddy
-const url = 'c:/inetpub/wwwroot/FroddiIdepro/ServicioPeticion' //Servidor Idepro
-// const url = 'C:/Users/Desktop/Desktop/Robotizacion/Nightwatch.JS' //Maquina Fer
+const url = process.cwd() //Servidor Idepro
 const urlImage = 'http://10.0.1.59:4500/reconocimiento/caracter'
 const time = '200000'
 let rutaFisa = `c:\\inetpub\\wwwroot\\FroddiIdepro\\Aplicacion\\recursos` // Servidor idepro
-// let rutaFisa = `c:\\sistemas\\FroddiUsuario\\public\\recursos` //Maquina Fer
 
 let port = 7000
 
